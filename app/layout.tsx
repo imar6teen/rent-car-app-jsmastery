b383f4abdb9fd4ce0bf6d04774769493e7f1861a
Aimar Anand <aimaranand02@gmail.com>
2023-06-26 13:02:44 +0800

    finish layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Footer, Navbar } from "@/components";
 
 export const metadata = {
   title: "Car Showcase",
@@ -15,7 +13,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,8 +27,8 @@ function Hero({}: Props) {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
-          <div className="hero__image-overlay"></div>
         </div>
+        <div className="hero__image-overlay"></div>
       </div>
     </div>
   );
