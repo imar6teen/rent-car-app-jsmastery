fa10f7140cedda463234126c121e2b875ed35b47
Aimar Anand <aimaranand02@gmail.com>
2023-06-29 16:28:23 +0800

    finishing car details modal

---
+++ utils/index.ts
@@ -0,0 +1,34 @@
+export async function fetchCars() {
+  const headers = {
+    "X-RapidAPI-Key": "3a28944755mshe9ba5ef9e6629a5p125346jsnc7e2664c3e92",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera",
+    {
+      headers: headers,
+      method: "GET",
+      //   next: { revalidate: 10 },
+    }
+  );
+
+  const result = await response.json();
+
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50;
+
+  const mileageFactor = 0.1;
+
+  const ageFactor = 0.05;
+
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
