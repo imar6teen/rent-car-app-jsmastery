c4e99fa4aa6b0a0b4c6ef4313b2c7f90a434634d
Aimar Anand <aimaranand02@gmail.com>
2023-07-05 18:21:05 +0800

    finish custom filter

--- utils/index.ts
+++ utils/index.ts
@@ -7,14 +7,15 @@ export async function fetchCars(filters: FilterProps) {
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
-  const response = await fetch(
-    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
-    {
-      headers: headers,
-      method: "GET",
-      //   next: { revalidate: 10 },
-    }
-  );
+  const url = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`;
+
+  console.log(url);
+
+  const response = await fetch(url, {
+    headers: headers,
+    method: "GET",
+    //   next: { revalidate: 10 },
+  });
 
   const result = await response.json();
 
@@ -50,4 +51,14 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   url.searchParams.append("angle", `${angle}`);
 
   return `${url}`;
+};
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type, value);
+
+  const newPathName = `${window.location.pathname}?${searchParams.toString()}`;
+
+  return newPathName;
 };
